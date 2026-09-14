c95fe486301876fdc84a3019a24f912091e7963f
klimshyk96-boop <klimshyk96@gmail.com>
2026-09-14 18:13:47 +0300

    Add files via upload

--- js/rooms/multiroom-lifecycle.js
+++ js/rooms/multiroom-lifecycle.js
@@ -45,8 +45,7 @@
       elemItems:clone(typeof elemItems!=='undefined'?elemItems:[]),
       elemGroups:clone(typeof elemGroups!=='undefined'?elemGroups:[]),
       lightMarks:clone(typeof lightMarks!=='undefined'?lightMarks:[]),
-      wallMarks:clone(typeof wallMarks!=='undefined'?wallMarks:[]),linearElements:clone(typeof linearElements!=='undefined'?linearElements:[]),wallTypes:clone(typeof wallTypes!=='undefined'?wallTypes:[]),arcPoints:clone(typeof arcPoints!=='undefined'?arcPoints:[]),
-      ceilingCornices:clone(Array.isArray(window.ceilingCornices)?window.ceilingCornices:[])
+      wallMarks:clone(typeof wallMarks!=='undefined'?wallMarks:[]),linearElements:clone(typeof linearElements!=='undefined'?linearElements:[]),wallTypes:clone(typeof wallTypes!=='undefined'?wallTypes:[]),arcPoints:clone(typeof arcPoints!=='undefined'?arcPoints:[])
     };
   }
   function resolveActive(arr){
